207cf68c700f2a0d9e217acde197bfc65ca61690
Sara Cohen <cohensara02@gmail.com>
2021-08-24 16:51:57 +0300

    add product

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const fs = require("fs");
 const express = require("express");
 const app = express();
+const { v4: uuidv4 } = require("uuid");
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello");
 });
+
 app.get("/products", (req, res) => {
   fs.readFile("./products.json", "utf8", (err, data) => {
     res.send(JSON.parse(data));
   });
 });
+
 app.get("/products/:id", (req, res) => {
   const { id } = req.params;
   fs.readFile("./products.json", "utf8", (err, data) => {
@@ -19,3 +23,23 @@ app.get("/products/:id", (req, res) => {
   });
 });
 app.listen(8000);
+
+app.post("/products", (req, res) => {
+  res.send("check");
+  const { title } = req.body;
+  fs.readFile("./products.json", "utf8", (err, data) => {
+    const products = JSON.parse(data);
+    const newProduct = {
+      //  id: uuidv4(),
+      id: products.length + 1,
+      title,
+      price: 39,
+      description: "beautiful jewelry",
+      category: "jewelery",
+      image:
+        "https://cashcow-cdn.azureedge.net/images/a45bec16-b540-45c5-9915-b5d14f42e467.jpg",
+    };
+    products.push(newProduct);
+    fs.writeFile("./products.json", JSON.stringify(products), (err) => {});
+  });
+});
